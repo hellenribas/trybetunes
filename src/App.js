8e11a0c873390fff544452f286199151eeecaa84
hellenribas <tchwtrabalho@gmail.com>
2022-03-07 18:27:37 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -7,9 +7,53 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import { createUser } from './services/userAPI';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputName: '',
+      isDesabled: true,
+      loadingApi: false,
+      redirect: false,
+    };
+  }
+
+  componentDidUpdate() {
+    // this.requestApi();
+  }
+
+  handleState = ({ target }) => {
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [target.name]: value,
+    }, () => {
+      const inputLength = 3;
+      const { inputName } = this.state;
+      const nameUser = inputName.replace(' ', '');
+      if (nameUser.length >= inputLength) {
+        this.setState({
+          isDesabled: false,
+        });
+      } else {
+        this.setState({
+          isDesabled: true,
+        });
+      }
+    });
+  }
+
+  requestApi = () => {
+    const { inputName } = this.state;
+    this.setState({ loadingApi: true }, async () => {
+      await createUser({ name: inputName });
+      this.setState({ loadingApi: false, redirect: true });
+    });
+  }
+
   render() {
+    const { isDesabled, inputName, loadingApi, redirect } = this.state;
     return (
       <BrowserRouter>
         <Switch>
@@ -18,7 +62,21 @@ class App extends React.Component {
           <Route exact path="/favorites" component={ Favorites } />
           <Route exact path="/profile" component={ Profile } />
           <Route exact path="/profile/edit" component={ ProfileEdit } />
-          <Route exact path="/" component={ Login } />
+          <Route
+            exact
+            path="/"
+            render={ (props) => (<Login
+              { ...props }
+              isDesabled={ isDesabled }
+              handleState={ this.handleState }
+              inputName={ inputName }
+              requestApi={ this.requestApi }
+              loadingApi={ loadingApi }
+            />
+            ) }
+          >
+            {redirect && <Redirect to="/search" /> }
+          </Route>
           <Route exact path="*" component={ NotFound } />
         </Switch>
       </BrowserRouter>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
+// import { Route, Redirect } from 'react-router-dom';
 
 class Login extends Component {
   render() {
+    const { isDesabled, inputName, handleState, requestApi, loadingApi } = this.props;
     return (
-      <div data-testid="page-login">Estou no Login</div>
+      <div data-testid="page-login">
+        {!loadingApi ? (
+          <form>
+            <input
+              type="text"
+              data-testid="login-name-input"
+              value={ inputName }
+              name="inputName"
+              onChange={ handleState }
+            />
+            <button
+              disabled={ isDesabled }
+              type="submit"
+              data-testid="login-submit-button"
+              onClick={ requestApi }
+            >
+              Entrar
+            </button>
+          </form>) : <h1>Carregando...</h1>}
+      </div>
     );
   }
 }
+
+Login.propTypes = ({
+  isDesabled: propTypes.bool,
+}).isRequired;
 
 export default Login;
