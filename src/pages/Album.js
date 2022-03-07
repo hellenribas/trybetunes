8539eb48287906662de3e01cb782cc0753e41f40
hellenribas <tchwtrabalho@gmail.com>
2022-03-07 15:25:51 -0300

    requisito 1

---
+++ src/pages/Album.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Album extends Component {
+  render() {
+    return (
+      <div data-testid="page-album">Estou no Album</div>
+    );
+  }
+}
+
+export default Album;
---
+++ src/pages/ProfileEdit.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ProfileEdit extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">Estou no ProfileEdit</div>
+    );
+  }
+}
+
+export default ProfileEdit;
