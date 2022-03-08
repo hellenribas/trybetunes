9ae9dd9b06479658417b40c587a17d743e051b0d
hellenribas <tchwtrabalho@gmail.com>
2022-03-08 09:02:11 -0300

    requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   constructor() {
@@ -7,6 +9,9 @@ class Search extends Component {
     this.state = {
       isDesabledButton: true,
       inputSearch: '',
+      loading: false,
+      result: '',
+      data: [],
     };
   }
 
@@ -28,28 +33,62 @@ class Search extends Component {
     });
   }
 
+  searchAlbum = () => {
+    const { inputSearch, data } = this.state;
+    this.setState({ result: inputSearch, inputSearch: '', loading: true }, async () => {
+      const arrData = await searchAlbumsAPI(inputSearch);
+      this.setState({
+        data: [...data, ...arrData],
+        loading: false,
+      });
+    });
+  }
+
   render() {
-    const { isDesabledButton, inputSearch } = this.state;
+    const { isDesabledButton, inputSearch, loading, result, data } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <input
-            type="text"
-            data-testid="search-artist-input"
-            value={ inputSearch }
-            name="inputSearch"
-            onChange={ this.searchInput }
-          />
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ isDesabledButton }
-          >
-            Pesquisar
-          </button>
-        </form>
-        <p>Estou no Search</p>
+        { loading ? 'Carregando...' : (
+          <form>
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              value={ inputSearch }
+              name="inputSearch"
+              onChange={ this.searchInput }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ isDesabledButton }
+              onClick={ this.searchAlbum }
+            >
+              Pesquisar
+            </button>
+          </form>)}
+        {data.length === 0 ? <p>Nenhum álbum foi encontrado</p> : (
+          <section>
+            <p>
+              {`Resultado de álbuns de: 
+              ${result}`}
+            </p>
+            <ul>
+              {data.map((album) => (
+                <li key={ `${album.collectionName} ${album.collectionId}` }>
+                  <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+                  <h3>{album.collectionName}</h3>
+                  <p>{album.artistName}</p>
+                  <Link
+                    to={ `/album/${album.collectionId}` }
+                    data-testid={ `link-to-album-${album.collectionId}` }
+                  >
+                    Album
+                  </Link>
+                </li>))}
+            </ul>
+          </section>
+        )}
       </div>
     );
   }
