92816a408ebcdb389ed903733862cad7eab038ac
hellenribas <tchwtrabalho@gmail.com>
2022-03-12 12:50:27 -0300

    requisito 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,33 +1,47 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
-// import { getUser } from '../services/userAPI';
+import { getUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Profile extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     userInfo: [],
-  //     loading: false,
-  //   };
-  // }
+  constructor() {
+    super();
+    this.state = {
+      userInfo: [],
+      loadingApi: false,
+    };
+  }
 
-  // componentDidMount() {
-  //   this.userApi();
-  // }
+  componentDidMount() {
+    this.userApi();
+  }
 
-  // userApi = () => {
-  //   this.setState({ loadingApi: true }), async () => {
-  //     const user = await getUser();
-  //     console.log(user);
-  //     this.setState({ loadingApi: false, userInfo: user });
-  // }
-  // }
+  userApi = () => {
+    this.setState({ loadingApi: true }, async () => {
+      const user = await getUser();
+      this.setState({ loadingApi: false, userInfo: user });
+    });
+  }
 
   render() {
+    const { loadingApi, userInfo: { description, email, image, name } } = this.state;
+
     return (
       <div data-testid="page-profile">
         <Header />
-        <p>EStou no Profile</p>
+        {loadingApi ? <Loading /> : (
+          <section>
+            <h2>Nome</h2>
+            <p>{name}</p>
+            <h2>Email</h2>
+            <p>{email}</p>
+            <h2>Descrição</h2>
+            <p>{description}</p>
+            <img data-testid="profile-image" src={ image } alt={ name } />
+            <Link to="/profile/edit">Editar perfil</Link>
+          </section>
+        )}
       </div>
     );
   }
